845db70dcfdaad590b9e2250afe6384fdfe838ae
Mats Tyldum <ma.tyldum@gmail.com>
2020-01-20 09:49:41 +0100

    First commit

---
+++ android/src/com/maattss/intro/exercise/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.maattss.intro.exercise;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.maattss.intro.exercise.IntroExercise;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IntroExercise(), config);
+	}
+}
